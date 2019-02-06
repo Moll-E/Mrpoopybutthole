99ab9819432b513f0f0a7ac17735eb083f4614cc
Parker Duncan <Duncan.parker.h@gmail.com>
2019-02-05 21:32:16 -0800

    Controls and Physics build 0.00000001
    LOL WUT

---
+++ Mrpoopybutthole/Intermediate/Build/Mac/UE4Editor/Inc/Mrpoopybutthole/JoyconOnePlayerController.gen.cpp
@@ -0,0 +1,77 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "Mrpoopybutthole/JoyconOnePlayerController.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeJoyconOnePlayerController() {}
+// Cross Module References
+	MRPOOPYBUTTHOLE_API UClass* Z_Construct_UClass_AJoyconOnePlayerController_NoRegister();
+	MRPOOPYBUTTHOLE_API UClass* Z_Construct_UClass_AJoyconOnePlayerController();
+	ENGINE_API UClass* Z_Construct_UClass_APlayerController();
+	UPackage* Z_Construct_UPackage__Script_Mrpoopybutthole();
+// End Cross Module References
+	void AJoyconOnePlayerController::StaticRegisterNativesAJoyconOnePlayerController()
+	{
+	}
+	UClass* Z_Construct_UClass_AJoyconOnePlayerController_NoRegister()
+	{
+		return AJoyconOnePlayerController::StaticClass();
+	}
+	struct Z_Construct_UClass_AJoyconOnePlayerController_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UE4CodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_AJoyconOnePlayerController_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_APlayerController,
+		(UObject* (*)())Z_Construct_UPackage__Script_Mrpoopybutthole,
+	};
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_AJoyconOnePlayerController_Statics::Class_MetaDataParams[] = {
+		{ "HideCategories", "Collision Rendering Utilities|Transformation" },
+		{ "IncludePath", "JoyconOnePlayerController.h" },
+		{ "ModuleRelativePath", "JoyconOnePlayerController.h" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_AJoyconOnePlayerController_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<AJoyconOnePlayerController>::IsAbstract,
+	};
+	const UE4CodeGen_Private::FClassParams Z_Construct_UClass_AJoyconOnePlayerController_Statics::ClassParams = {
+		&AJoyconOnePlayerController::StaticClass,
+		DependentSingletons, ARRAY_COUNT(DependentSingletons),
+		0x009002A4u,
+		nullptr, 0,
+		nullptr, 0,
+		"Game",
+		&StaticCppClassTypeInfo,
+		nullptr, 0,
+		METADATA_PARAMS(Z_Construct_UClass_AJoyconOnePlayerController_Statics::Class_MetaDataParams, ARRAY_COUNT(Z_Construct_UClass_AJoyconOnePlayerController_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_AJoyconOnePlayerController()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			UE4CodeGen_Private::ConstructUClass(OuterClass, Z_Construct_UClass_AJoyconOnePlayerController_Statics::ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(AJoyconOnePlayerController, 3843858646);
+	static FCompiledInDefer Z_CompiledInDefer_UClass_AJoyconOnePlayerController(Z_Construct_UClass_AJoyconOnePlayerController, &AJoyconOnePlayerController::StaticClass, TEXT("/Script/Mrpoopybutthole"), TEXT("AJoyconOnePlayerController"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AJoyconOnePlayerController);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ Mrpoopybutthole/Intermediate/Build/Mac/UE4Editor/Inc/Mrpoopybutthole/JoyconOnePlayerController.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef MRPOOPYBUTTHOLE_JoyconOnePlayerController_generated_h
+#error "JoyconOnePlayerController.generated.h already included, missing '#pragma once' in JoyconOnePlayerController.h"
+#endif
+#define MRPOOPYBUTTHOLE_JoyconOnePlayerController_generated_h
+
+#define Mrpoopybutthole_Source_Mrpoopybutthole_JoyconOnePlayerController_h_15_RPC_WRAPPERS
+#define Mrpoopybutthole_Source_Mrpoopybutthole_JoyconOnePlayerController_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Mrpoopybutthole_Source_Mrpoopybutthole_JoyconOnePlayerController_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAJoyconOnePlayerController(); \
+	friend struct Z_Construct_UClass_AJoyconOnePlayerController_Statics; \
+public: \
+	DECLARE_CLASS(AJoyconOnePlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Mrpoopybutthole"), NO_API) \
+	DECLARE_SERIALIZER(AJoyconOnePlayerController)
+
+
+#define Mrpoopybutthole_Source_Mrpoopybutthole_JoyconOnePlayerController_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAJoyconOnePlayerController(); \
+	friend struct Z_Construct_UClass_AJoyconOnePlayerController_Statics; \
+public: \
+	DECLARE_CLASS(AJoyconOnePlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Mrpoopybutthole"), NO_API) \
+	DECLARE_SERIALIZER(AJoyconOnePlayerController)
+
+
+#define Mrpoopybutthole_Source_Mrpoopybutthole_JoyconOnePlayerController_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AJoyconOnePlayerController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AJoyconOnePlayerController) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AJoyconOnePlayerController); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AJoyconOnePlayerController); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AJoyconOnePlayerController(AJoyconOnePlayerController&&); \
+	NO_API AJoyconOnePlayerController(const AJoyconOnePlayerController&); \
+public:
+
+
+#define Mrpoopybutthole_Source_Mrpoopybutthole_JoyconOnePlayerController_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AJoyconOnePlayerController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AJoyconOnePlayerController(AJoyconOnePlayerController&&); \
+	NO_API AJoyconOnePlayerController(const AJoyconOnePlayerController&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AJoyconOnePlayerController); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AJoyconOnePlayerController); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AJoyconOnePlayerController)
+
+
+#define Mrpoopybutthole_Source_Mrpoopybutthole_JoyconOnePlayerController_h_15_PRIVATE_PROPERTY_OFFSET
+#define Mrpoopybutthole_Source_Mrpoopybutthole_JoyconOnePlayerController_h_12_PROLOG
+#define Mrpoopybutthole_Source_Mrpoopybutthole_JoyconOnePlayerController_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Mrpoopybutthole_Source_Mrpoopybutthole_JoyconOnePlayerController_h_15_PRIVATE_PROPERTY_OFFSET \
+	Mrpoopybutthole_Source_Mrpoopybutthole_JoyconOnePlayerController_h_15_RPC_WRAPPERS \
+	Mrpoopybutthole_Source_Mrpoopybutthole_JoyconOnePlayerController_h_15_INCLASS \
+	Mrpoopybutthole_Source_Mrpoopybutthole_JoyconOnePlayerController_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Mrpoopybutthole_Source_Mrpoopybutthole_JoyconOnePlayerController_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Mrpoopybutthole_Source_Mrpoopybutthole_JoyconOnePlayerController_h_15_PRIVATE_PROPERTY_OFFSET \
+	Mrpoopybutthole_Source_Mrpoopybutthole_JoyconOnePlayerController_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Mrpoopybutthole_Source_Mrpoopybutthole_JoyconOnePlayerController_h_15_INCLASS_NO_PURE_DECLS \
+	Mrpoopybutthole_Source_Mrpoopybutthole_JoyconOnePlayerController_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Mrpoopybutthole_Source_Mrpoopybutthole_JoyconOnePlayerController_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
